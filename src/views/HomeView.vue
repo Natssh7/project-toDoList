f3a6284ac0b63cfef3411903c988f9ac784976d8
Nathan Tran <nathan.clement78@gmail.com>
2024-03-19 09:37:49 -0400

    seventh commit milestone3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,21 @@
         <div class="flex-initial relative">
           <div class="min-h-[53rem] bg-my-image bg-no-repeat bg-cover bg-center bg-fixed z-0 absolute inset-0 flex-shrink-0"></div>
         </div>
-        <div class="bg-gray-700 z-5 inset-y-0 left-0 h-screen w-1/3 flex flex-col items-center justify-center">
-          <div>
+        <div class="bg-gray-700 z-5 inset-y-0 left-0 h-screen w-1/3 flex flex-col items-center justify-center pb-36">
+          <div class="my-4">
+            <span class="block text-center justify-center text-7xl text-gray-100 mb-4">To-Do List</span>
+            <span class="block text-center justify-center text-4xl text-gray-100">Simplified</span>
+          </div>
+          <div class="my-4">
             <router-link
               :to="{ name: 'login' }"
-              class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-[75px] rounded focus:outline-none focus:shadow-outline"
+              class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-32 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Log In
             </router-link>
           </div>
-          <div class="mt-4">
+          <div class="my-4">
             <router-link :to="{ name: 'signin' }" class="block text-gray-200 hover:text-gray-800 text-sm font-bold mb-2">Don't have an account? Sign In</router-link>
           </div>
         </div>
